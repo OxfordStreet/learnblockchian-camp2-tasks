92e210f472e69c97e26c7a8475c381d2a3759316
OxfordStreet <32759302+OxfordStreet@users.noreply.github.com>
2023-04-04 16:18:19 +0800

    reconfig privite key and polygon apikey

--- w1-2/hardhat.config.js
+++ w1-2/hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("hardhat-abi-exporter");
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -12,12 +13,12 @@ module.exports = {
     mumbai: {
       url: "https://rpc-mumbai.maticvigil.com",
       chainId: 80001,
-      accounts: [`0x10ccce74f9893ff3bed704f116f73e6d75dcd02ac22259d68c9525d240d1eab0`]
+      accounts: [process.env.PRIVATE_KEY]
     }
   },
   etherscan: {
     apiKey: {
-      polygonMumbai: '1WBEBRGY6RSS4NYX9INIK8E8E57PSSTQQY'
+      polygonMumbai: process.env.POLYGONSCAN_API_KEY
     }
   },
   abiExporter: {
